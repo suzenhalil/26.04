f8a815a3e9df6c2f965ebbc0ab53205d33bc45f0
suzenhalil <suzenhalil@hotmail.com>
2022-05-11 12:33:31 +0300

    gg

---
+++ PersonelTakip/Araclar.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace PersonelTakip
+{
+    partial class Araclar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMarka = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.txtRenk = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbKasa = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbVites = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cmbYakit = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cmbDonanim = new System.Windows.Forms.ComboBox();
+            this.txtMotor = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtGuvenlik = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Marka";
+            // 
+            // txtMarka
+            // 
+            this.txtMarka.Location = new System.Drawing.Point(107, 18);
+            this.txtMarka.Name = "txtMarka";
+            this.txtMarka.Size = new System.Drawing.Size(128, 23);
+            this.txtMarka.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Model";
+            // 
+            // txtModel
+            // 
+            this.txtModel.Location = new System.Drawing.Point(107, 55);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(128, 23);
+            this.txtModel.TabIndex = 3;
+            // 
+            // txtRenk
+            // 
+            this.txtRenk.Location = new System.Drawing.Point(107, 93);
+            this.txtRenk.Name = "txtRenk";
+            this.txtRenk.Size = new System.Drawing.Size(128, 23);
+            this.txtRenk.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Renk";
+            // 
+            // cmbKasa
+            // 
+            this.cmbKasa.FormattingEnabled = true;
+            this.cmbKasa.Location = new System.Drawing.Point(107, 131);
+            this.cmbKasa.Name = "cmbKasa";
+            this.cmbKasa.Size = new System.Drawing.Size(128, 23);
+            this.cmbKasa.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Kasa";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 172);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Vites";
+            // 
+            // cmbVites
+            // 
+            this.cmbVites.FormattingEnabled = true;
+            this.cmbVites.Location = new System.Drawing.Point(107, 169);
+            this.cmbVites.Name = "cmbVites";
+            this.cmbVites.Size = new System.Drawing.Size(128, 23);
+            this.cmbVites.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(31, 365);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 28);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "KAYDET";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(33, 214);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Yakit";
+            // 
+            // cmbYakit
+            // 
+            this.cmbYakit.FormattingEnabled = true;
+            this.cmbYakit.Location = new System.Drawing.Point(107, 211);
+            this.cmbYakit.Name = "cmbYakit";
+            this.cmbYakit.Size = new System.Drawing.Size(128, 23);
+            this.cmbYakit.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(33, 253);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 15);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Donanim";
+            // 
+            // cmbDonanim
+            // 
+            this.cmbDonanim.FormattingEnabled = true;
+            this.cmbDonanim.Location = new System.Drawing.Point(107, 250);
+            this.cmbDonanim.Name = "cmbDonanim";
+            this.cmbDonanim.Size = new System.Drawing.Size(128, 23);
+            this.cmbDonanim.TabIndex = 13;
+            // 
+            // txtMotor
+            // 
+            this.txtMotor.Location = new System.Drawing.Point(107, 288);
+            this.txtMotor.Name = "txtMotor";
+            this.txtMotor.Size = new System.Drawing.Size(128, 23);
+            this.txtMotor.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(31, 295);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 15);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Motor";
+            // 
+            // txtGuvenlik
+            // 
+            this.txtGuvenlik.Location = new System.Drawing.Point(107, 326);
+            this.txtGuvenlik.Name = "txtGuvenlik";
+            this.txtGuvenlik.Size = new System.Drawing.Size(128, 23);
+            this.txtGuvenlik.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(31, 333);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 15);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Guvenlik";
+            // 
+            // Araclar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 460);
+            this.Controls.Add(this.txtGuvenlik);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtMotor);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cmbDonanim);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cmbYakit);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cmbVites);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbKasa);
+            this.Controls.Add(this.txtRenk);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtModel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMarka);
+            this.Controls.Add(this.label1);
+            this.Name = "Araclar";
+            this.Text = "Arac";
+            this.Load += new System.EventHandler(this.Araclar_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMarka;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtModel;
+        private System.Windows.Forms.TextBox txtRenk;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbKasa;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbVites;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cmbYakit;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cmbDonanim;
+        private System.Windows.Forms.TextBox txtMotor;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtGuvenlik;
+        private System.Windows.Forms.Label label9;
+    }
+}
---
+++ PersonelTakip/Araclar.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonelTakip
+{
+    public partial class Araclar : Form
+    {
+        public enum KasaTipi
+        {
+            Cupe,
+            Sedan,
+            Cabrio,
+            Hatchback,
+            StationWagon
+        }
+        public enum YakitTipi
+        {
+            Benzin,
+            Motorin,
+            Elektrik,
+            Hybrid
+
+        }
+        public enum Vites
+        {
+            Manuel,
+            YariOtomatik,
+            Otomatik
+        }
+        public enum DonanimPaketi
+        {
+            Full,
+            FullArtiFull,
+            Bos
+        }
+        struct Araba
+        {
+            public string Marka;
+            public string Model;
+            public string Renk;
+            public KasaTipi Kasa;
+            public Vites Vites;
+            public string GuvenlikPaketi;
+            public string Motor;
+            public YakitTipi Yakit;
+            public DonanimPaketi Paket;
+
+        }
+        public Araclar()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Araclar_Load(object sender, EventArgs e)
+        {
+            // Donanim PAketlerini Ekliyoruz
+            cmbDonanim.Items.AddRange(Enum.GetNames(typeof(DonanimPaketi)));
+            cmbDonanim.SelectedIndex = 0;
+
+            //Kasa Tiplerini ComboBox'a Ekliyoruz
+            cmbKasa.Items.AddRange(Enum.GetNames(typeof(KasaTipi)));
+            cmbKasa.SelectedIndex = 0;
+
+            // Yakit Tipini Ekliyoruz
+            cmbYakit.Items.AddRange(Enum.GetNames(typeof(YakitTipi)));
+            cmbYakit.SelectedIndex = 0;
+
+            // Vites Tipini Ekliyoruz
+            cmbVites.Items.AddRange(Enum.GetNames(typeof(Vites)));
+            cmbVites.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Araba arac = new Araba();
+
+            arac.Kasa = KasaTipi.Sedan;
+            arac.Yakit = YakitTipi.Benzin;
+        }
+    }
+}
